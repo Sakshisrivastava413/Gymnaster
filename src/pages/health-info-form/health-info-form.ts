8576bbab045ca86ba0e19ca40ac1d86ba8d112fc
sakshi <sakshisrivastava413@gmail.com>
2018-02-14 15:54:10 +0530

    Uploaded files

--- src/pages/health-info-form/health-info-form.ts
+++ src/pages/health-info-form/health-info-form.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { NativePageTransitions } from '@ionic-native/native-page-transitions';
+
 @IonicPage()
 @Component({
   selector: 'page-health-info-form',
@@ -8,11 +10,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HealthInfoFormPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user = {
+    name: '',
+    gender: ''
+  } as {
+    name: string;
+    gender: string;
+    dob: string;
+    weight: number;
+    height: number;
+    goal: number;
+  };
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public nTransitions: NativePageTransitions) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HealthInfoFormPage');
+  }
+
+  ionViewWillLeave() {
+    let options: any = {
+      direction: 'up',
+      duration: 500,
+      slowdownfactor: 3,
+      slidePixels: 20,
+      iosdelay: 100,
+      androiddelay: 150,
+      fixedPixelsTop: 0,
+      fixedPixelsBottom: 60
+    };
+    this.nTransitions.slide(options);
+  }
+
+  goToHome() {
+    this.navCtrl.setRoot('HomePage');
   }
 
 }
